e078b63b528ee2d7b1cdf69a8132eb066c5d9b4f
Olivier Guittonneau <olivier.g@outlook.com>
2022-11-02 00:49:17 +0100

    Add SSBO shader and test
    Also refacto of the test...

---
+++ tests/resources/shaders/texture_v5.vert
@@ -0,0 +1,19 @@
+#version 450
+#extension GL_ARB_separate_shader_objects : enable
+
+layout(binding = 0) readonly buffer SSBO
+{
+    mat4 mvp;
+} ssbo;
+
+layout(location = 0) in vec3 position;
+layout(location = 1) in vec2 texCoord;
+
+layout(location = 0) out vec2 out_texCoord;
+
+void main()
+{
+    gl_Position = ssbo.mvp * vec4(position, 1);
+
+    out_texCoord = texCoord;
+}
--- tests/src/all_combined.c
+++ tests/src/all_combined.c
@@ -4,6 +4,18 @@
 #include <stdlib.h>
 
 
+static void setModelMatrix(renderable renderable, float x, float y, float angle)
+{
+	mat4 matrix;
+
+	glm_mat4_identity(matrix);
+	glm_translate_x(matrix, x);
+	glm_translate_y(matrix, y);
+	glm_rotate_z(matrix, angle, matrix);
+	renderableSetMatrix(renderable, matrix);
+}
+
+
 int main(void)
 {
 	const int width = 640;
@@ -40,6 +52,15 @@ int main(void)
 	params.vertShaderName = "texture_v4.vert.spv";
 	params.fragShaderName = "texture_v3.frag.spv";
 	renderable room_push_cst_mvp = modelLoad("viking_room.obj", &params, 1, 0);
+	params.vertShaderName = "texture_v5.vert.spv";
+	params.fragShaderName = "texture_v2.frag.spv";
+	params.sendMVPAsPushConstant = 0;
+	params.sendMVPAsStorageBuffer = 1;
+	renderable room_sbbo = modelLoad("viking_room.obj", &params, 1, 0);
+	params.vertShaderName = "texture_v4.vert.spv";
+	params.fragShaderName = "texture_v3.frag.spv";
+	params.sendMVPAsPushConstant = 1;
+	params.sendMVPAsStorageBuffer = 0;
 	params.textureName = "holes.png";
 	renderable sphere_holes = modelLoad("sphere.obj", &params, 1, 0);
 	params.textureName = "this file doesn't exist";
@@ -49,53 +70,15 @@ int main(void)
 	{
 		float angle = glm_rad(getUptime() * 20);
 
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, -3);
-		glm_translate_y(matrix, -3);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(room, matrix);
-
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, -1);
-		glm_translate_y(matrix, -3);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(room_indexed, matrix);
-
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, 1);
-		glm_translate_y(matrix, -3);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(room_normals, matrix);
-
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, 3);
-		glm_translate_y(matrix, -3);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(room_indexed_normals, matrix);
-
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, -3);
-		glm_translate_y(matrix, -1);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(room_compact_mvp, matrix);
-
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, -1);
-		glm_translate_y(matrix, -1);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(room_push_cst_mvp, matrix);
-
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, 1);
-		glm_translate_y(matrix, -1);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(sphere_holes, matrix);
-
-		glm_mat4_identity(matrix);
-		glm_translate_x(matrix, 3);
-		glm_translate_y(matrix, -1);
-		glm_rotate_z(matrix, angle, matrix);
-		renderableSetMatrix(sphere_missing_texture, matrix);
+		setModelMatrix(room, -3, -3, angle);
+		setModelMatrix(room_indexed, -1, -3, angle);
+		setModelMatrix(room_normals, 1, -3, angle);
+		setModelMatrix(room_indexed_normals, 3, -3, angle);
+		setModelMatrix(room_compact_mvp, -3, -1, angle);
+		setModelMatrix(room_push_cst_mvp, -1, -1, angle);
+		setModelMatrix(room_sbbo, 1, -1, angle);
+		setModelMatrix(sphere_holes, 3, -1, angle);
+		setModelMatrix(sphere_missing_texture, -3, 1, angle);
 
 		denymRender();
 		denymWaitForNextFrame();
