fc591e5cb63e0f0172fe9c6106d5d0327314e74c
Olivier Guittonneau <olivier.g@outlook.com>
2022-11-29 02:29:31 +0100

    Update sprite example with two rows

--- tests/src/sprite.c
+++ tests/src/sprite.c
@@ -11,7 +11,7 @@ int main(void)
 	if (denymInit(width, height))
 		return EXIT_FAILURE;
 
-    sprite sprite = spriteCreate("mario_walk.png", 1, 2, 3, 1);
+    sprite sprite = spriteCreate("mario_walk.png", 1, 2, 3, 2);
 
 	vec3 eye = {0, -0.01f, 5};
 	vec3 center = { 0, 0, 0};
@@ -20,14 +20,17 @@ int main(void)
 	sceneSetCamera(denymGetScene(), camera);
     primitiveCreateGrid(8, 3);
 
+	uint32_t spriteConfig[] = { 2, 3 };
+
 	while(denymKeepRunning(NULL))
 	{
 		float time = getUptime();
 		float pos = sinf(time * 1.5f) * 4 - 0.5f;
+		int currentSprite = (int)(time / 5) % 2;
 
 		spriteFlip(sprite, pos < 0, 0);
 		spriteSetPosition(sprite, pos, 0);
-		spriteSetSpriteCoordinates(sprite, (uint32_t)(time * 15) % 3, 0);
+		spriteSetSpriteCoordinates(sprite, (uint32_t)(time * 10) % spriteConfig[currentSprite], currentSprite);
 		denymRender();
 		denymWaitForNextFrame();
 	}
